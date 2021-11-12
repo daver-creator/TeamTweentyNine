7ea58bda7bd0f7bbb1608d701e288533dac905da
daver-creator <55607493+daver-creator@users.noreply.github.com>
2021-11-12 18:13:19 -0500

    Update Models.py
    Added user

--- Models.py
+++ Models.py
@@ -9,3 +9,12 @@ class Question(db.Model):
         self.title = title
         self.text = text
         self.date = date
+        
+class User(db.Model):
+    id = db.Column("id",db.Integer,primary_key=True)
+    name = db.Column("name",db.String(100))
+    email = db.Column("email",db.String(100))
+
+    def __init__(self,name,email):
+        self.name = name
+        self.email = email
